374a02d973209e036dc7619ff42dc0e32c1f4750
FeatureSmith25 <hardiksachan25@gmail.com>
2026-07-21 23:34:07 +0530

    RAG based concepts

--- RAG Project/Document loader/text_loader.py
+++ RAG Project/Document loader/text_loader.py
@@ -3,6 +3,7 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+from langchain_core.runnables import RunnablePassthrough, RunnableSequence, RunnableParallel, RunnableLambda
 load_dotenv()
 
 model=ChatMistralAI()
@@ -23,4 +24,13 @@ docs=loader.load()
 
 chain=prompt | model | parser
 
-print(chain.invoke({'poem':docs[0].page_content}))
+parallel_chain=RunnableParallel({
+    'poem':RunnablePassthrough(),
+    'word_count':RunnableLambda(lambda x:len(x.split()))
+})
+# print(chain.invoke({'poem':docs[0].page_content}))
+
+final_chain=chain | parallel_chain
+result=final_chain.invoke({'poem':docs[0].page_content})
+print(result['poem'])
+print(result['word_count'])
